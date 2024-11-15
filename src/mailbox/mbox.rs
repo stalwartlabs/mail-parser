d0be05061bc77365e058792f3a15558e07da5612
c <c@farsight.net>
2024-11-15 12:34:48 +0100

    Hoist if across if let

--- src/mailbox/mbox.rs
+++ src/mailbox/mbox.rs
@@ -61,18 +61,18 @@ where
 
             let is_from = message_line.starts_with(b"From ");
 
-            if let Some(message) = &mut self.message {
-                if is_from && self.message.is_some() {
-                    // self.message is Some here, so message is Some
-                    let message = self.message.take().map(Ok);
-                    self.message =
-                        Message::new(std::str::from_utf8(&message_line).unwrap_or("")).into();
-                    // branch is always executed due to comment above
-                    // but will not compile because borrow checker doesnt know this
-                    // if message.is_some() {
-                        return message;
-                    // }
+            if is_from && self.message.is_some() {
+                // self.message is Some here, so message is Some
+                let message = self.message.take().map(Ok);
+                self.message =
+                    Message::new(std::str::from_utf8(&message_line).unwrap_or("")).into();
+                // branch is always executed due to comment above
+                if message.is_some() {
+                    return message;
                 }
+            }
+
+            if let Some(message) = &mut self.message {
                 if message_line[0] != b'>' {
                     message.contents.extend_from_slice(&message_line);
                 } else if message_line
