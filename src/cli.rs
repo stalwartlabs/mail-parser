9062be8babbc35e8e2f331cf8c97622521134df6
Clément DOUIN <clement.douin@posteo.net>
2026-04-08 17:46:58 +0200

    docs: add missing license headers

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2020 Stalwart Labs LLC <hello@stalw.art>
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ */
+
 use std::{
     fs,
     io::{stdin, BufRead, IsTerminal},
